69e622d9a0a07f64d66b347de8065c1849eca71c
haruto kawata <solxyz1927@gmail.com>
2024-10-12 22:46:50 +0900

    first commit

---
+++ src/main/kotlin/com/example/springstudykotlin/SpringStudyKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springstudykotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringStudyKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringStudyKotlinApplication>(*args)
+}
---
+++ src/main/kotlin/com/example/springstudykotlin/controller/HelloController.kt
@@ -0,0 +1,12 @@
+package com.example.springstudykotlin.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HelloController {
+    @GetMapping("/")
+    fun hello(): String {
+        return "hello"
+    }
+}
